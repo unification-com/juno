feb5db300af34c47680eff1db9b08b5dc9fc61aa
Magic Cat <pusz.monika@gmail.com>
2023-05-03 15:21:03 +0700

    replace tendermint with cometbft pkg

--- node/builder/builder.go
+++ node/builder/builder.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/forbole/juno/v4/node"
 	nodeconfig "github.com/forbole/juno/v4/node/config"
-	"github.com/forbole/juno/v4/node/local"
+	// "github.com/forbole/juno/v4/node/local"
 	"github.com/forbole/juno/v4/node/remote"
 )
 
@@ -16,7 +16,8 @@ func BuildNode(cfg nodeconfig.Config, encodingConfig *params.EncodingConfig) (no
 	case nodeconfig.TypeRemote:
 		return remote.NewNode(cfg.Details.(*remote.Details), encodingConfig.Codec)
 	case nodeconfig.TypeLocal:
-		return local.NewNode(cfg.Details.(*local.Details), encodingConfig.TxConfig, encodingConfig.Codec)
+		return nil, nil
+		// return local.NewNode(cfg.Details.(*local.Details), encodingConfig.TxConfig, encodingConfig.Codec)
 	case nodeconfig.TypeNone:
 		return nil, nil
 
